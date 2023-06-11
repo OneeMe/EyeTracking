e2140e5ecf724d695adc497bc00ce200fe604f24
onee <onee.me@outlook.com>
2023-06-12 06:32:22 +0800

    feat: 添加最初版本的功能

--- EyeTracking/ContentView.swift
+++ EyeTracking/ContentView.swift
@@ -7,31 +7,80 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    @StateObject var lookAtPointData = LookAtPointData()
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack {
+            ARViewContainer(lookAtPointData: lookAtPointData).edgesIgnoringSafeArea(.all)
+            GeometryReader { geo in
+                Circle()
+                    .fill(.red)
+                    .position(lookAtPointData.point ?? .zero)
+                    .frame(width: 20, height: 20)
+                    .fixedSize()
+            }
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
+    @ObservedObject var lookAtPointData: LookAtPointData
+    
     func makeUIView(context: Context) -> ARView {
         
+        let config = ARFaceTrackingConfiguration()
+        config.worldAlignment = .camera
+        
+        #if targetEnvironment(simulator)
         let arView = ARView(frame: .zero)
+        #else
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: false)
+        arView.session.run(config, options: [.removeExistingAnchors])
+        arView.session.delegate = context.coordinator
+        #endif
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        context.coordinator.view = arView
+        context.coordinator.lookAtPointData = lookAtPointData
         
         return arView
-        
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func makeCoordinator() -> FaceCoordinator {
+        return FaceCoordinator()
+    }
     
+    func updateUIView(_ uiView: ARView, context: Context) {
+        // update
+    }
+    
+}
+
+class FaceCoordinator: NSObject, ARSessionDelegate {
+    weak var view: ARView!
+    weak var lookAtPointData: LookAtPointData!
+    
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        guard let faceAnchor = anchors.first(where: { $0 is ARFaceAnchor }) as? ARFaceAnchor else { return }
+        
+        let facePosition = simd_float3(
+            faceAnchor.transform.columns.3.x,
+            faceAnchor.transform.columns.3.y,
+            faceAnchor.transform.columns.3.z
+        )
+        
+        let lookAtPointInWorld = facePosition + faceAnchor.lookAtPoint
+        let lookAtPointInView = view.project(lookAtPointInWorld)
+        
+        lookAtPointData.point = lookAtPointInView
+    }
+}
+
+class LookAtPointData: ObservableObject {
+    @Published var point: CGPoint?
 }
 
 #if DEBUG
